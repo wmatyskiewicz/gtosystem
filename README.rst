a072aae97998d7dd7c7b110d689435c63061a0ff
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2013-06-20 22:01:46 +0200

    added South package

--- README.rst
+++ README.rst
@@ -4,4 +4,6 @@ gtosystem install
 1. git clone git@github.com:wmatyskiewicz/gtosystem.git gtosystem
 2. virtualenv . --distribute --no-site-packages
 3. pip install -r requirements.txt
+4. ./manage.py syncdb
+
 
--- project/gtosystem/gtosystem/settings.py
+++ project/gtosystem/gtosystem/settings.py
@@ -14,13 +14,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'gtosystem.db',
         'USER': '',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -124,6 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'south',
     'core',
 )
 
